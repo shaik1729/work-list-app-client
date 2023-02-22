68890f6c337589fcfaa4ac2048ace71355cbd49e
shaik1729 <shaiktajuddinshavali@gmail.com>
2023-02-22 11:53:43 +0530

    navbar with routing has been implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-// import Tasks from './components/Tasks';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import AddTask from './components/AddTask';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Header from './components/Header'
+import AddTask from './components/AddTask'
+import Tasks from './components/Tasks'
 
 function App() {
   return (
-    <AddTask />
+    <>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/add-task" element={<AddTask />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
